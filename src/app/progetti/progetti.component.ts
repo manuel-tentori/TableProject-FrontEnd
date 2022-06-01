ffcb3b44d575756bde24c4e7e40b76fe75b961f0
Manuel <manuel.tentori@gmail.com>
2022-06-01 12:01:07 +0200

    commit

---
+++ src/app/progetti/progetti.component.ts
@@ -0,0 +1,178 @@
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Custom } from "../template";
+
+declare var $: any;
+
+@Component({
+  selector: "app-progetti",
+  templateUrl: "./progetti.component.html",
+  styleUrls: ["./progetti.component.css"],
+})
+export class ProgettiComponent implements OnInit {
+  isShowDiv = false;
+0
+  public flagTab = false;
+  public flagAperta = false;
+  public flagApertaAddProject = false;
+  public showMyContainer: boolean = false;
+
+  public newData: Data = new Data();
+
+
+  public datas: any[] = [
+    {
+      ID: 1,
+      Title: "Progetto 1",
+      Revenue: 1000,
+      DateStart: new Date(2022, 1, 1).getTime(),
+      DateEnd: new Date(2022, 12, 31).getTime(),
+      Weeks: [
+        {
+          id: 1,
+          ProgressPercWeek: null,
+          PartialRevenue: null,
+        },
+      ]
+    },
+    {
+      ID: 2,
+      Title: "Progetto 2",
+      Revenue: 3450,
+      DateStart: new Date(2022, 1, 1).getTime(),
+      DateEnd: new Date(2022, 12, 31).getTime(),
+      Weeks: [
+        {
+          id: 1,
+          ProgressPercWeek: null,
+          PartialRevenue: null,
+        },
+      ]
+    },
+    {
+      ID: 3,
+      Title: "Progetto 3",
+      Revenue: 44250,
+      DateStart: new Date(2022, 1, 1).getTime(),
+      DateEnd: new Date(2022, 12, 31).getTime(),
+      Weeks: [
+        {
+          id: 1,
+          ProgressPercWeek: null,
+          PartialRevenue: null,
+        },
+      ]
+    },
+    {
+      ID: 4,
+      Title: "Progetto 4",
+      Revenue: 2134,
+      DateStart: new Date(2022, 1, 1).getTime(),
+      DateEnd: new Date(2022, 12, 31).getTime(),
+      Weeks: [
+        {
+          id: 1,
+          ProgressPercWeek: null,
+          PartialRevenue: null,
+        },
+      ]
+    },
+    {
+      ID: 5,
+      Title: "Progetto 5",
+      Revenue: 9658,
+      DateStart: new Date(2022, 1, 1).getTime(),
+      DateEnd: new Date(2022, 12, 31).getTime(),
+      Weeks: [
+        {
+          id: 1,
+          ProgressPercWeek: null,
+          PartialRevenue: null,
+        },
+      ]
+    }
+  ];
+
+  ngOnInit() {
+    $("#perc").keyup(function () {
+      var value = $(this).val();
+    });
+
+  }
+
+  AddWeek() {
+    const w = new Week();
+    let i = 0;
+    w.ID = i;
+    w.ProgressPercWeek = null;
+    w.PartialRevenue = null;
+
+    this.datas.forEach((dd) => {
+      dd.Weeks.push(w);
+    });
+  }
+
+  Sum(d: Data): number {
+    let sum = 0;
+    d.Weeks.forEach((w) => {
+      sum += w.PartialRevenue;
+    });
+
+    return sum;
+  }
+
+  ProgressPerc(d: Data): number {
+    let sum = 0;
+    d.Weeks.forEach((w) => {
+      sum += w.ProgressPercWeek;
+    });
+
+    return sum;
+  }
+
+  PartialRevenue(d: Data): number {
+    let partial;
+    d.Weeks.forEach((w) => {
+      partial = (d.Revenue / 100) * w.ProgressPercWeek;
+    });
+    return partial;
+  }
+
+  OpenAddProject() {
+    this.flagApertaAddProject = true;
+  }
+
+  back() {
+    this.flagAperta = false;
+    this.flagApertaAddProject = false;
+  }
+
+  AddProject() {
+    const DataClone = Object.assign([], this.newData);
+    let w = new Week();
+    w.ProgressPercWeek = null;
+    w.PartialRevenue = null;
+    this.newData.Weeks.push(w);
+    this.datas.push(DataClone);
+  }
+}
+
+export class Week {
+  ID: number;
+  ProgressPercWeek: number;
+  PartialRevenue: number;
+}
+
+export class Data {
+  ID: number;
+  Title: string;
+  Revenue: number;
+  ProgessPerc: number;
+  DateStart: number;
+  DateEnd: number;
+  Weeks: Week[];
+
+  constructor() {
+    this.Weeks = [];
+  }
+}
---
+++ src/app/template.ts
@@ -0,0 +1,7 @@
+export class Custom {
+    id: number;
+    Title: string;
+    Revenue: number;
+    DateStart: number;
+    DateEnd: number;
+}
